333f1b19cc58802d55296aa3977124501b880324
Tomas Trescak <tomi.trescak@gmail.com>
2015-10-10 20:14:28 +1100

    Automatic creation of app now works.

--- bin/metpack.js
+++ bin/metpack.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-// if (process.argv.length < 2) {
+require('shelljs/global');
+var dirs = require('../lib/dirs.js');
+var path = require('path');
 
-// }
-
-console.log(JSON.stringify(process.argv));
+//console.log(JSON.(process.argv));
 
 if (process.argv.length == 2 || process.argv[2] == "dev") {
   var dev = require("../lib/dev.js").run;
@@ -12,6 +12,40 @@ if (process.argv.length == 2 || process.argv[2] == "dev") {
 } else if (process.argv[2] == "prod") {
   var run = require("../lib/prod.js").run;
   run();
+} if (process.argv[2] == "create") {
+  // copy package.json to project dir
+  if (!process.argv[3]) {
+    console.log("Please specify the name of the project");
+    return;
+  }
+
+  var appDir = path.join(dirs.app, process.argv[3])
+  var libDir = path.join(dirs.root, 'lib');
+
+  console.log("Creating directory ...");
+
+  // create a project directory
+  mkdir("-p", path.join(appDir, '.webpack'));
+  cd(appDir);
+
+  console.log("Scaffolding application ...");
+
+  // copy package json
+  cp(path.join(libDir, "package.json"), path.join(appDir, 'package.json'));
+  cp(path.join(dirs.root, ".babelrc"), path.join(appDir, '.babelrc'));
+
+  // copy meteor directory
+  cp('-R', path.join(libDir, 'meteor_core'), appDir);
+  cp('-R', path.join(libDir, 'settings'), appDir);
+  cp('-R', path.join(libDir, 'app'), appDir);
+
+  console.log("Installing NPM modules (this can take several minutes, please be patient) ...");
+
+  // run npm install
+  exec('npm install');
+
+  // Build core.js from project directory 4 copy meteor, example app
+  // Done
 } else {
   console.log("TODO: Give info on how the plugin is used");
   console.log("metpack or metpack dev: runs a Metpack instance in development mode");
